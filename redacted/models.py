0fce8b68d8998b5e6f836d78ce7a3487a2730465
Jeremy Carbaugh <jcarbaugh@gmail.com>
2012-07-18 14:59:05 -0400

    make popup work... finally

--- redacted/models.py
+++ redacted/models.py
@@ -2,10 +2,12 @@ import datetime
 
 from django.db import models
 
+
 class Post(models.Model):
 
     title = models.CharField(max_length=255)
-    description = models.TextField(help_text="This field is not displayed to users", blank=True)
+    description = models.TextField(blank=True)
+    scribd_embed = models.TextField(blank=True)
     date_posted = models.DateField(default=datetime.date.today)
     url = models.URLField(verify_exists=False)
     thumbnail = models.ImageField(upload_to='thumbnails')
--- redacted/urls.py
+++ redacted/urls.py
@@ -1,20 +1,38 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import ListView
+from django.http import HttpResponseRedirect
+from django.views.generic import DetailView, ListView
 
 from redacted.models import Post
 
 admin.autodiscover()
+
 
 class RedactedView(ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'index.html'
 
+
+class PostView(DetailView):
+    model = Post
+
+    def get(self, request, *args, **kwargs):
+
+        resp = super(PostView, self).get(request, *args, **kwargs)
+
+        if request.is_ajax():
+            return resp
+        else:
+            post = resp.context_data['post']
+            return HttpResponseRedirect(post.url)
+
+
 urlpatterns = patterns('',
-    url(r'^$', RedactedView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^post/(?P<pk>\d+)/$', PostView.as_view(), name='post_detail'),
+    url(r'^$', RedactedView.as_view(), name='post_list'),
     url(r'', include('sfapp.urls')),
 )
 
